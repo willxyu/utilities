637425193ba2ff421c3c203feb4d1c03fd2ea096
willxyu <willxyu@users.noreply.github.com>
2024-02-15 05:43:41 +1100

    Update utilities.js

--- javascript/utilities.js
+++ javascript/utilities.js
@@ -1,8 +1,21 @@
+String.prototype.toProperCase = function () {
+  return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
 
 utilities = (function() {
   // Access global definitions here if required
  
   // Start functions
+  let addCSS = function(rule, container, ruleIdentifier) {
+    let rc = ruleIdentifier ? ruleIdentifier : 'customCSS'
+    let output = '<span class="' + rc + '">&shy;<style>' + rule + '</style></span>'
+    document.querySelectorAll(rc).forEach(e => e.remove())
+    if (container) {
+      document.querySelector(container).insertAdjacentHTML('beforeend', output)
+    } else {
+      document.body.insertAdjacentHTML('beforeend', output)
+    }
+  }
   /* Extracts numbers provided in an alphanumeric string in a contiguous fashion */
   let clean = function(n) { if (typeof n !== 'string') { return n }; return Number(n.replace(/[^-\d\.]/g,'')) }
   
@@ -32,6 +45,17 @@ utilities = (function() {
       parts[0]  = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g,',')
       return parts.join('.') }
       
+  let degreesToRadians = function(degrees) { return Math.PI * degrees / 180 }
+
+  let getDistance = function(a,b) { 
+    return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2)) 
+  }
+
+  let getAngleFromPoint = function(p, cp) {
+    let a = ((Math.atan2(p.y - cp.y, p.x - cp.x) * 180) / Math.PI) % 360
+    return a = a < 0 ? 360 + a : a
+  }
+  
   /* Calculate time between two Date() objects */
   let interval = function(a, b) {
       if (!a) { return false }
@@ -53,6 +77,50 @@ utilities = (function() {
   /* Retrieve the key that maps to a provided value */
   let key = function(obj, v) { for (var prop in obj) { if (obj.hasOwnProperty(prop)) { if (obj[prop] === v) { return prop } } } }
 
+  let raiseEvent = function(target, event, datum) { return target.dispatchEvent(new CustomEvent(event, {detail: datum})) };
+
+  /* Random */
+  let random = function(ceiling, min) {
+     if (min && min > ceiling) { [ceiling, min] = [min, ceiling] }
+     let c = ceiling || 1
+     let m = min     || 0
+     return (c - m) * Math.random() + m 
+  }
+
+  let randomNormal = function() {
+     let u = 0;
+     let v = 0;
+     while(u === 0) { u = Math.random() } // Converting [0,1) to (0,1)
+     while(v === 0) { v = Math.random() }
+     return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+  }
+  
+  /* https://stackoverflow.com/a/49434653 */
+  let randomRange = function(min, max, skew) {
+     if (min && min > max) { [max, min] = [min, max] }
+     var skew = skew || 1;
+     let u = 0, v = 0;
+     while(u === 0) u = Math.random() //Converting [0,1) to (0,1)
+     while(v === 0) v = Math.random()
+     let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )
+  
+     num = num / 10.0 + 0.5 // Translate to 0 -> 1
+     if (num > 1 || num < 0) 
+       num = randomRange(min, max, skew) // resample between 0 and 1 if out of range
+     else{
+       num  = Math.pow(num, skew) // Skew
+       num *= max - min // Stretch to fill range
+       num += min // offset to min
+     }
+     return num
+  }
+  
+  let randomFromObject = function(obj) {
+    let keys = Object.keys(obj)
+    let key  = keys[Math.floor(Math.random() * keys.length)]
+    return [obj[key], key]
+  }
+
   /* Rounds numbers */
   let round = function(num, scale) {
       if (!('' + num).includes('e')) { 
@@ -61,6 +129,17 @@ utilities = (function() {
         let arr = ('' + num).split('e')
         return + (Math.round(+arr[0] + 'e' + ((+arr[1] + scale > 0) ? '+' : '') + (+arr[1] + scale)) + 'e-' + scale)
       }
+  }
+  
+  /* https://stackoverflow.com/a/2450976 */
+  let shuffleArray = function(arr) {
+    let ci = arr.length, ri;
+    while(ci > 0) {
+      ri = Math.floor(Math.random() * ci)
+      ci--
+      [arr[ci], arr[ri]] = [arr[ri], arr[ci]]
+    }
+    return arr
   }
 
   /* https://stackoverflow.com/a/1584377 */
@@ -85,15 +164,48 @@ utilities = (function() {
         return (v == 'x' ? r : (r&0x3|0x8)).toString(16)
       })
   }
+  
+  /* Special sorting functions */
+  let chainsort = function(arr) {
+    return function(a, b) {
+      for (var i = 0; i < arr.length; i++) {
+        var r = arr[i](a, b)
+        if (r != 0) { return r }
+      }
+      return 0
+    }
+  }
+  let sort_by = function(field, reverse, primer) {
+    var key = primer ? function(x) { return primer(x[field]) } : function(x) { return x[field] }
+    reverse = [-1, 1][+!!reverse]
+    return function(a, b) {
+      a = key(a)
+      b = key(b)
+      return a == b ? 0 : reverse * ((a > b) - (b > a))
+    }
+  }
 
   return {
-    clean      :  clean,
-    clone      :  clone,
-    commaThis  :  commaThis,
-    interval   :  interval,
-    key        :  key,
-    round      :  round,
-    uniqueArray:  uniqueArray,
-    uuid       :  uuid,
+    addCSS            :  addCSS,
+    clean             :  clean,
+    clone             :  clone,
+    commaThis         :  commaThis,
+    degreesToRadians  :  degreesToRadians,
+    getDistance       :  getDistance,
+    getAngleFromPoint :  getAngleFromPoint,
+    interval          :  interval,
+    key               :  key,
+    raiseEvent        :  raiseEvent,
+    random            :  random,
+    randomNormal      :  randomNormal,
+    randomRange       :  randomRange,
+    randomFromObject  :  randomFromObject,
+    round             :  round,
+    shuffleArray      :  shuffleArray,
+    uniqueArray       :  uniqueArray,
+    uuid              :  uuid,
+    // sorting functions
+    chainsort         :  chainsort,
+    sort_by           :  sort_by,
   }
 })()
